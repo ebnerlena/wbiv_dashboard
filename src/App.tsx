1e80800673b818d0eaf38541dfc5fbda6d13ddf6
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2022-12-22 04:04:29 +0100

    Add more dashboard like layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -7,26 +6,17 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <h1 className="header__title">Solar Engery</h1>
+      </header>
+      <main className="content">
+        <div>
+          <button onClick={() => setCount(count => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </main>
+      <footer className="footer">Lena Ebner - WBIV Dashboard WS 2022</footer>
     </div>
   )
 }
