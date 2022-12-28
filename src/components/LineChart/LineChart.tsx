ecb15c91dcfa0c8054ade24f0b376b290d761d9b
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2022-12-28 13:32:29 +0100

    Update linechart to use yearly data only

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -1,17 +1,22 @@
 import { parse } from 'papaparse'
 import { useEffect, useState } from 'react'
 import Plot from 'react-plotly.js'
-import './LineChart.css'
 
-type PVData = {
+type LineChartProps = {
+  id: string
+  year: number
+}
+
+type DataMapping = {
   x: number[]
   y: number[]
 }
 
-const LineChart = () => {
+const LineChart: React.FC<LineChartProps> = ({ id, year }) => {
   // line chart from 1980 to 2019
 
-  const [data, setData] = useState<PVData | null>(null)
+  const [data, setData] = useState<DataMapping | null>(null)
+  const [yearData, setYearData] = useState<DataMapping | null>(null)
 
   useEffect(() => {
     parse('data/ninja_pv_country_at.csv', {
@@ -30,37 +35,48 @@ const LineChart = () => {
         const xDataDaily: number[] = []
         const yDataDaily: number[] = []
 
+        const myYear = year
+
         let i = 0
         let j = 0
         let medianXDaily: number[] = []
-        let avgXDaily: number = 0
+        let xDailyForYear: number[] = []
+        let yDailyForYear: number[] = []
+        let dailyAvg: number = 0
 
         data.slice(3).forEach((entry: any) => {
-          medianXDaily.push(entry[1])
-          avgXDaily += entry[1]
+          const curDate = new Date(entry[0])
+          const curYear = curDate.getFullYear()
           xDataAll.push(entry[0])
           yDataAll.push(entry[1])
 
-          if (i == 24) {
-            xDataDaily.push(entry[0])
-            // yDataDaily.push(avgXDaily / 24)
-            yDataDaily.push(
-              medianXDaily.sort()[Math.round(medianXDaily.length / 2)]
-            ) // use median to ignore 0 outliers
-            i = 0
-            j++
-            medianXDaily = []
-            avgXDaily = 0
+          if (curYear == myYear) {
+            medianXDaily.push(entry[1])
+            dailyAvg += entry[1]
+            // yDailyForYear.push(entry[1])
+            // xDailyForYear.push(entry[0])
+
+            if (i == 23) {
+              xDailyForYear.push(entry[0])
+              //yDailyForYear.push(Math.max(...medianXDaily))
+              yDailyForYear.push(dailyAvg / 24)
+              // yDailyForYear.push(
+              //   medianXDaily.sort()[Math.round(medianXDaily.length / 2)]
+              // ) // use median to ignore 0 outliers
+              i = 0
+              j++
+              medianXDaily = []
+              dailyAvg = 0
+            }
+            i++
           }
-          i++
         })
 
         const dataMapping = {
-          x: xDataDaily,
-          y: yDataDaily,
-        } as PVData
+          x: xDailyForYear,
+          y: yDailyForYear,
+        } as DataMapping
 
-        console.log(dataMapping)
         setData(dataMapping)
       },
     })
@@ -70,9 +86,10 @@ const LineChart = () => {
     <div className="linechart">
       {data && (
         <Plot
-          className="linchart__plot"
+          divId={`linechart-${id}`}
+          className="linechart__plot"
           useResizeHandler={true}
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: '45%', height: '100%' }}
           data={[
             {
               x: data.x,
@@ -88,11 +105,23 @@ const LineChart = () => {
             showEditInChartStudio: false,
             showSources: false,
             responsive: true,
+            scrollZoom: true,
           }}
           layout={{
-            title: 'Daily PV Production in AT from 1980 - 2019',
-            font: { size: 14 },
+            title: 'Yearly AVG PV Production Capacity',
+            font: { size: 10 },
             autosize: true,
+            height: 400,
+            yaxis: {
+              title: 'avg capacity',
+              zeroline: false,
+            },
+            margin: {
+              l: 60,
+              r: 30,
+              b: 30,
+              t: 80,
+            },
           }}
         />
       )}
