b95e414ff4d7f7929a61766133a3920027509225
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2022-12-28 13:31:18 +0100

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import BoxPlot from './components/BoxPlot/BoxPlot'
 import LineChart from './components/LineChart/LineChart'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [year, setYear] = useState<number>(2017)
+  console.log('Selected year: ', year)
 
   return (
     <div className="App">
@@ -11,14 +13,18 @@ function App() {
         <h1 className="header__title">Solar Engery</h1>
       </header>
       <main className="content">
-        <LineChart />
-        <div>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is {count}
-          </button>
+        <div className="content__top">
+          <LineChart id="pv-daily" year={year} />
+        </div>
+        <div className="content__bottom">
+          {/* <BarChart id="pv-yearly" /> */}
+          <BoxPlot
+            id="pv-yearly"
+            selectYear={(year: number) => setYear(year)}
+          />
         </div>
       </main>
-      <footer className="footer">Lena Ebner - WBIV Dashboard WS 2022</footer>
+      {/* <footer className="footer">Lena Ebner - WBIV Dashboard WS 2022</footer> */}
     </div>
   )
 }
